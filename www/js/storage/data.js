9c514866d6b6190fbe6fec01cc62fff747a53df7
Nick Garland <ngarland1@gmail.com>
2014-10-28 23:51:09 -0700

    Fixing data

--- www/js/storage/data.js
+++ www/js/storage/data.js
@@ -1,9 +1,19 @@
 var data = {
-    pterosaurs: [ {
-            name: "Azhdarcho",
-            image1: "azhdarchos.png"
+  pterosaurs: [ {
+      id: 1,
+      name: "Azhdarcho",
+      description: "Azhdarcho is a genus of pterodactyloid pterosaur from the late Cretaceous Period of the Bissekty Formation (middle Turonian stage, about 92 million years ago) of Uzbekistan. It is known from fragmentary remains including the distinctive, elongated neck vertebrae that characterizes members of the family Azhdarchidae, which also includes such giant pterosaurs as Quetzalcoatlus. The name Azhdarcho comes from the Persian word azhdarha, the name of a dragon in Persian mythology. The type species is Azhdarcho lancicollis. The specific epithet lancicollis is derived from the Latin words lancea (meaning 'lance' or 'spear') and collum 'neck').",
+      geography: "Uzbekistan",
+      geology: "Cretaceous",
+      family: "Azhdarchidae",
+      image1: "azhdarcho.jpg"
     }, {
-            name: "Pterodactylus",
-            image1: "pterodactylus.png"
+      id: 2,
+      name: "Pterodactylus",
+      description: "Pterodactylus is a genus of pterosaurs whose members are popularly known as pterodactyls. It is currently thought to contain only a single species, Pterodactylus antiquus, the first pterosaur species to be named and identified as a flying reptile. The fossil remains of this species have been found primarily in the Solnhofen limestone of Bavaria, Germany, dated to the late Jurassic Period (early Tithonian), about 150.8–148.5 million years ago,[2] though more fragmentary remains have been tentatively identified from elsewhere in Europe and in Africa. It was a carnivore and probably preyed upon fish and other small animals. Like all pterosaurs, the wings of Pterodactylus were formed by a skin and muscle membrane stretching from its elongated fourth finger to its hind limbs. It was supported internally by collagen fibres and externally by keratinous ridges.",
+      geography: "Germany, Europe, Africa",
+      geology: "Jurassic",
+      family: "Pterodactylidae",
+      image1: "dorygnathus.jpg"
     } ]
 };
